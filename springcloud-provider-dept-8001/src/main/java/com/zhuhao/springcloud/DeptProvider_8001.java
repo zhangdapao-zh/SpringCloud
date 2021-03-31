1f1ddcc9a495bee52cf3435d0df1fafd84b0c4f3
zh <702021619@qq.com>
2021-03-31 12:32:51 +0800

    1

--- springcloud-provider-dept-8001/src/main/java/com/zhuhao/springcloud/DeptProvider_8001.java
+++ springcloud-provider-dept-8001/src/main/java/com/zhuhao/springcloud/DeptProvider_8001.java
@@ -2,8 +2,12 @@ package com.zhuhao.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient   //在服务启动后自动注册到Eureka中
+@EnableDiscoveryClient
 public class DeptProvider_8001 {
     public static void main(String[] args) {
         SpringApplication.run(DeptProvider_8001.class,args);
--- springcloud-provider-dept-8001/src/main/java/com/zhuhao/springcloud/controller/DeptController.java
+++ springcloud-provider-dept-8001/src/main/java/com/zhuhao/springcloud/controller/DeptController.java
@@ -4,6 +4,8 @@ package com.zhuhao.springcloud.controller;
 import com.zhuhao.springcloud.pojo.Dept;
 import com.zhuhao.springcloud.service.DeptService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,16 +15,46 @@ public class DeptController {
     @Autowired
     private DeptService deptService;
 
+    @Autowired
+    private DiscoveryClient client;
+
     @PostMapping("/dept/add")
-    public boolean addDept(Dept dept){
+    public boolean addDept(Dept dept) {
         return deptService.addDept(dept);
     }
+
     @GetMapping("/dept/get/{id}")
-    public Dept getDept(@PathVariable("id") Long id){
+    public Dept getDept(@PathVariable("id") Long id) {
         return deptService.queryById(id);
     }
+
     @GetMapping("/dept/list")
     public List<Dept> queryAll() {
         return deptService.queryAll();
     }
+
+
+    @GetMapping("/dept/discovery")
+    public Object discovery() {
+
+        //获取微服务的列表清单
+        List<String> services = client.getServices();
+        System.out.println("services ++++" + services);
+
+        //得到一个具体的微服务信息，通过serviceId
+        List<ServiceInstance> instances = client.getInstances("SPRINGCLOUD-PROVIDER-DEPT");
+
+        for (ServiceInstance instance : instances) {
+            System.out.println(
+                    instance.getHost() + "\t" +
+                            instance.getPort() + "\t" +
+                            instance.getUri() + "\t" +
+                            instance.getServiceId()
+
+            );
+        }
+        return this.client;
+    }
+
+
 }
